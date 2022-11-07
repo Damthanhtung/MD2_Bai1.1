307f1bf25865a084bd2844ff5d872f94469a0501
Đamthanhtung <tungdtha140465@fpt.edu.vn>
2022-11-07 10:21:47 +0700

    su dung spread attributes

---
+++ bai 1/demo.js
@@ -0,0 +1,14 @@
+// Example 1
+// const cars1 = ["AUDI","BMW","TATA","MERCEDES"];
+// const cars2 = [...cars1];
+// const cars3 = [...cars1,"NISSAN","TOYOTA"];
+// console.log(cars1);
+// console.log(cars2);
+// console.log(cars3);
+
+// Example 2
+const cars1 = ["AUDI","BMW","TATA","MERCEDES"];
+const cars2 = ["NISAN","TOYOTA"];
+const cars3 = [...cars1,...cars2];
+console.log(cars3);
+  
